3306a715e34b6ba5e95c33acf098c1e8afdada21
JoeKerouac <guanhua.qgh@antfin.com>
2020-03-08 21:16:00 +0800

    更改main方法，测试

--- src/main/java/com/github/JoeKerouac/nativenet/Main.java
+++ src/main/java/com/github/JoeKerouac/nativenet/Main.java
@@ -1,11 +1,18 @@
 package com.github.JoeKerouac.nativenet;
 
+import com.github.JoeKerouac.nativenet.nativ.NativeArpNetInterface;
+import com.github.JoeKerouac.nativenet.nativ.impl.NativeArpNetInterfaceImpl;
+
 /**
  * @author JoeKerouac
  * @version 2020年03月08日 17:27
  */
 public class Main {
     public static void main(String[] args) {
+        NativeArpNetInterface nativeArpNetInterface = new NativeArpNetInterfaceImpl();
+        int sock = nativeArpNetInterface.createSock();
+        System.out.println("sock 是 " + sock);
+        nativeArpNetInterface.close(sock);
         System.out.println("成功了");
     }
 }

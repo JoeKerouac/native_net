9ce993e4c5cfa6fa46fb36d78d0186ca1fdcdb86
JoeKerouac <guanhua.qgh@antfin.com>
2020-03-24 22:22:10 +0800

    更新代码

--- src/main/java/com/github/JoeKerouac/nativenet/Main.java
+++ src/main/java/com/github/JoeKerouac/nativenet/Main.java
@@ -26,6 +26,13 @@ public class Main {
         nativeNetFilterInterface.register(data -> {
             IpPacket ipPackage = new IpPacket(data.getData());
 
+            if (!NetStringUtils.toIpString(ipPackage.getDestAdd()).equals("192.168.199.130")){
+                System.out.println("源ip是：" + NetStringUtils.toIpString(ipPackage.getSrcAdd()));
+                System.out.println("目标ip是：" + NetStringUtils.toIpString(ipPackage.getDestAdd()));
+                System.out.println("协议是：" + ipPackage.getSubProtocol());
+                System.out.println("对象是：" + ipPackage.getSubPackage());
+            }
+
             if (ipPackage.getSubPackage() != null) {
                 TcpSegment tcpPackage = ipPackage.getSubPackage();
                 // 只打印80端口的数据

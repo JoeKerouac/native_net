1a7df144c3be120939e5ab9577d283a1553b0955
JoeKerouac <guanhua.qgh@antfin.com>
2020-03-26 00:15:06 +0800

    写死queueNum调试

--- src/main/resources/native/com_github_JoeKerouac_nativenet_nativ_impl_NativeNetFilterInterfaceImpl.c
+++ src/main/resources/native/com_github_JoeKerouac_nativenet_nativ_impl_NativeNetFilterInterfaceImpl.c
@@ -242,8 +242,10 @@ JNIEXPORT void JNICALL Java_com_github_JoeKerouac_nativenet_nativ_impl_NativeNet
   (JNIEnv *env, jobject nativeNetFilterInterfaceImpl, jobject netFilterCallbackData, jint verdict){
     // 申请内存，后边要释放
     struct callback_data *data = data_convert_to_c(env, netFilterCallbackData);
-    nfuq_send_verdict(nfuq_read_queue_num(data), nfuq_read_id(data), nfuq_read_data_len(data),
+    nfuq_send_verdict(0, nfuq_read_id(data), nfuq_read_data_len(data),
       nfuq_read_data(data), verdict);
+//    nfuq_send_verdict(nfuq_read_queue_num(data), nfuq_read_id(data), nfuq_read_data_len(data),
+//          nfuq_read_data(data), verdict);
     // 释放内存
     free(nfuq_read_data(data));
     free_callback_data(data);

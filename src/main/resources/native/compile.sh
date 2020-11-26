c4199cae8f297ba9e5568a69501ddaa171772526
JoeKerouac <1213812243@qq.com>
2020-11-26 18:25:28 +0800

    更新编译脚本

--- src/main/resources/native/compile.sh
+++ src/main/resources/native/compile.sh
@@ -1,8 +1,8 @@
 # 注意，本地库编译依赖的arp_request_lib.o和nf_userspace_queue.o这两个私有库需要自己编译，源码在clib库中
-# 编译上述两个库的时候需要使用gcc -fPIC -shared 命令来编译
+# 编译上述两个库的时候需要使用gcc -fPIC -shared 命令来编译，同时-l选项也是必不可少的，不然链接库会找不到（不加的话只会从当前目录查找）
 
 
 # 编译arp本地库
-gcc -fPIC -shared com_github_JoeKerouac_nativenet_nativ_impl_NativeArpNetInterfaceImpl.c arp_request_lib.o -o libcom_github_JoeKerouac_nativenet_nativ_impl_NativeArpNetInterfaceImpl.so
+gcc -fPIC -shared com_github_JoeKerouac_nativenet_nativ_impl_NativeArpNetInterfaceImpl.c -larp_request_lib -o libcom_github_JoeKerouac_nativenet_nativ_impl_NativeArpNetInterfaceImpl.so
 # 编译netfilter本地库
-gcc -fPIC -shared com_github_JoeKerouac_nativenet_nativ_impl_NativeNetFilterInterfaceImpl.c nf_userspace_queue.o -lmnl -lnetfilter_queue -o libcom_github_JoeKerouac_nativenet_nativ_impl_NativeNetFilterInterfaceImpl.so
+gcc -fPIC -shared com_github_JoeKerouac_nativenet_nativ_impl_NativeNetFilterInterfaceImpl.c -lnf_userspace_queue -o libcom_github_JoeKerouac_nativenet_nativ_impl_NativeNetFilterInterfaceImpl.so
